3b55f991361346bf1a6a40f541a61152b2b87043
Matej Gomboc <matejg03@gmail.com>
2023-01-29 16:42:22 +0100

    Cleanup main.cpp

--- main.cpp
+++ main.cpp
@@ -1,53 +1,42 @@
 #include <SDKDDKVer.h>
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include <CommCtrl.h>
-#include <cstdlib>
-#include "renderer.h"
 
-static Simulator::Renderer renderer;
+#include "renderer.h"
 
 static LRESULT CALLBACK wndProc(HWND window, UINT message, WPARAM wparam, LPARAM lparam)
 {
 	switch (message) {
 	case WM_CREATE: {
 		auto create_info = reinterpret_cast<CREATESTRUCTA*>(lparam);
-
-		HWND devices_combobox = CreateWindow(WC_COMBOBOX, TEXT("DevicesCombobox"),
-			CBS_DROPDOWNLIST | CBS_HASSTRINGS | WS_CHILD | WS_OVERLAPPED | WS_VISIBLE,
-			0, 0, 1000, 1000, window, nullptr, create_info->hInstance, nullptr);
-
-		if (devices_combobox == nullptr) {
-			MessageBoxA(nullptr, "Cannot create devices combo box.", "ERROR", MB_ICONERROR | MB_OK | MB_APPLMODAL);
+		if (create_info == nullptr) {
 			return -1;
 		}
 
-		LRESULT error = SendMessage(devices_combobox, CB_ADDSTRING, 0, reinterpret_cast<LPARAM>(TEXT("1")));
-		if ((error == CB_ERR) || (error == CB_ERRSPACE)) {
-			MessageBoxA(nullptr, "Cannot create devices combo box.", "ERROR", MB_ICONERROR | MB_OK | MB_APPLMODAL);
+		auto renderer = reinterpret_cast<Simulator::Renderer*>(create_info->lpCreateParams);
+		if (renderer == nullptr) {
 			return -1;
 		}
 
-		error = SendMessage(devices_combobox, CB_ADDSTRING, 0, reinterpret_cast<LPARAM>(TEXT("2")));
-		if ((error == CB_ERR) || (error == CB_ERRSPACE)) {
-			MessageBoxA(nullptr, "Cannot create devices combo box.", "ERROR", MB_ICONERROR | MB_OK | MB_APPLMODAL);
+		SetLastError(0);
+		if (SetWindowLongPtr(window, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(renderer)) != 0) {
 			return -1;
 		}
 
-		/*error = SendMessage(devices_combobox, CB_SETCURSEL, 0, 0);
-		if ((error == CB_ERR) || (error == CB_ERRSPACE)) {
-			MessageBoxA(nullptr, "Cannot create devices combo box.", "ERROR", MB_ICONERROR | MB_OK | MB_APPLMODAL);
+		if (GetLastError() != ERROR_SUCCESS) {
 			return -1;
-		}*/
+		}
 
-		return 0;
-	}
-	case WM_CLOSE: {
-		DestroyWindow(window);
 		return 0;
 	}
 	case WM_DESTROY: {
-		PostQuitMessage(EXIT_SUCCESS);
+		auto renderer = reinterpret_cast<Simulator::Renderer*>(GetWindowLongPtr(window, GWLP_USERDATA));
+		if (renderer == nullptr) {
+			PostQuitMessage(GetLastError());
+			return 0;
+		}
+
+		PostQuitMessage(ERROR_SUCCESS);
 		return 0;
 	}
 	default:
@@ -55,24 +44,19 @@ static LRESULT CALLBACK wndProc(HWND window, UINT message, WPARAM wparam, LPARAM
 	}
 }
 
-int APIENTRY wWinMain(_In_ HINSTANCE app_instance, _In_opt_ HINSTANCE prev_app_instance,
-	_In_ LPWSTR cmd_line, _In_ int cmd_show)
+int APIENTRY wWinMain(_In_ HINSTANCE app_instance, _In_opt_ HINSTANCE prev_app_instance, _In_ LPWSTR cmd_line, _In_ int cmd_show)
 {
 	UNREFERENCED_PARAMETER(prev_app_instance);
 	UNREFERENCED_PARAMETER(cmd_line);
 
-	std::string out_error_message;
-	if (!renderer.init(out_error_message)) {
-		MessageBoxA(nullptr, out_error_message.c_str(), "ERROR", MB_ICONERROR | MB_OK | MB_APPLMODAL);
-		return EXIT_FAILURE;
-	}
+	Simulator::Renderer renderer;
 
-	WNDCLASSEXW window_class{};
+	WNDCLASSEX window_class{};
 	window_class.cbSize = sizeof(WNDCLASSEX);
 	window_class.style = CS_HREDRAW | CS_VREDRAW;
 	window_class.lpfnWndProc = wndProc;
 	window_class.cbClsExtra = 0;
-	window_class.cbWndExtra = 0;
+	window_class.cbWndExtra = sizeof(LONG_PTR);
 	window_class.hInstance = app_instance;
 	window_class.hIcon = nullptr;
 	window_class.hCursor = LoadCursor(nullptr, IDC_ARROW);
@@ -82,19 +66,17 @@ int APIENTRY wWinMain(_In_ HINSTANCE app_instance, _In_opt_ HINSTANCE prev_app_i
 	window_class.hIconSm = nullptr;
 
 	if (RegisterClassEx(&window_class) == 0) {
-		MessageBoxA(nullptr, "Cannot register window class.", "ERROR", MB_ICONERROR | MB_OK | MB_APPLMODAL);
-		return EXIT_FAILURE;
+		return GetLastError();
 	}
 
-	HWND window = CreateWindow(TEXT("MainWindow"), TEXT("Simulator"), WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, nullptr, nullptr, app_instance, nullptr);
+	HWND main_window = CreateWindow(TEXT("MainWindow"), TEXT("Simulator"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, nullptr, nullptr, app_instance, &renderer);
 
-	if (window == nullptr) {
-		MessageBoxA(nullptr, "Cannot create main window.", "ERROR", MB_ICONERROR | MB_OK | MB_APPLMODAL);
-		return EXIT_FAILURE;
+	if (main_window == nullptr) {
+		return GetLastError();
 	}
 
-	ShowWindow(window, cmd_show);
+	ShowWindow(main_window, cmd_show);
 
 	MSG message;
 	while (GetMessage(&message, nullptr, 0, 0)) {
@@ -102,6 +84,5 @@ int APIENTRY wWinMain(_In_ HINSTANCE app_instance, _In_opt_ HINSTANCE prev_app_i
 		DispatchMessage(&message);
 	}
 
-	renderer.destroy();
 	return (int)message.wParam;
 }
